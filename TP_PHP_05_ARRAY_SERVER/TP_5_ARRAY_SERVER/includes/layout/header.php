0c4a7bdcbf0ff08d4459ccac07e4b63acaa4a344
Davy Blavette <davy.blavette@akairnet.com>
2020-06-24 12:32:08 +0200

    TP 24 juin - Array, variable, KeyExist

---
+++ TP_PHP_05_ARRAY_SERVER/TP_5_ARRAY_SERVER/includes/layout/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html class="no-js" lang="fr" prefix="og: http://ogp.me/ns#">
+<head>
+    <!-- Basic Page Needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <title><?php echo $title; ?></title>
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta property="og:title" content="<?php echo $title; ?>" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="<?php echo 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']; ?>" />
+    <meta property="og:image" content="<?php echo 'http://'.$_SERVER['SERVER_NAME'].$root; ?>/img/<?php echo $ogImg ?>" />
+
+    <link href="./css/main.css<?php echo "?v=".time() ?>" rel="stylesheet" />
+
+
+</head>
+<body>
+
+<nav>
+    <ul>
+        <li><a href="index.php" class="<?php echo $page == 'accueil' ? 'active' : ''; ?>">Accueil</a></li>
+        <li><a href="index.php?page=activites" class="<?php echo $page == 'activites' ? 'active' : ''; ?>">Activite</a></li>
+        <li><a href="index.php?page=contact" class="<?php echo $page == 'contact' ? 'active' : ''; ?>">Contact</a></li>
+    </ul>
+</nav>
