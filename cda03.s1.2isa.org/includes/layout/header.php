a0819a02396cef5e190d7de9f9ceefeb1460bf1c
Davy Blavette <davy.blavette@akairnet.com>
2020-06-24 10:31:03 +0200

    Array(title, description), KeyExist

--- cda03.s1.2isa.org/includes/layout/header.php
+++ cda03.s1.2isa.org/includes/layout/header.php
@@ -2,11 +2,11 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <meta name="description" content="Gutim Template">
-    <meta name="keywords" content="Gutim, unica, creative, html">
+    <meta name="description" content="<?php echo $description ?>">
+    <meta name="keywords" content="<?php echo $keywords ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Gutim | Template</title>
+    <title><?php echo $title ?></title>
 
     <!-- Google Font -->
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900&display=swap"
--- cda03.s1.2isa.org/index.php
+++ cda03.s1.2isa.org/index.php
@@ -5,18 +5,63 @@
 //pour afficher les informations du variable
 //var_dump($_GET['page']);
 
-//test si la clef de l'url existe
+$ar_pages_var = array(
+
+    'accueil' => array(
+        'title' => 'Moto Club Millau Passion | Accueil',
+        'description' => 'Moto Club Millau Passion | Association de loisir autour de la moto !',
+        'keywords' => 'Moto, Plaisir, Club, Millau, Passion, Accueil',
+        'meteo' => 1
+    ),
+    'activites' => array(
+        'title' => 'Moto Club Millau Passion | Nos activités',
+        'description' => 'Les activités de l\'association Moto Club Millau Passion',
+        'keywords' => 'Activtés, Tarn, Deux roues, Millau, Passion, Accueil',
+        'meteo' => NULL
+
+    ),
+    'contact' => array(
+        'title' => 'Moto Club Millau Passion | Nous contacter',
+        'description' => 'Les activités de l\'association Moto Club Millau Passion',
+        'keywords' => 'Activtés, Tarn, Deux roues, Millau, Passion, Accueil'
+    ),
+    'galerie' => array(
+        'title' => 'Moto Club Millau Passion | Notre Galerie Photo',
+        'description' => 'Les activités de l\'association Moto Club Millau Passion',
+        'keywords' => 'Activtés, Tarn, Deux roues, Millau, Passion, Accueil'
+    ),
+    'informations' => array(
+        'title' => 'Moto Club Millau Passion | Les informations de l\'association',
+        'description' => 'Les activités de l\'association Moto Club Millau Passion',
+        'keywords' => 'Activtés, Tarn, Deux roues, Millau, Passion, Accueil'
+    ),
+    'presentation' => array(
+        'title' => 'Moto Club Millau Passion | Présentation et histoire de làssociation',
+        'description' => 'Les activités de l\'association Moto Club Millau Passion',
+        'keywords' => 'Activtés, Tarn, Deux roues, Millau, Passion, Accueil'
+    ),
+);
+
+//valeur par default
+$page = 'accueil';
+
+//test si la clef de l'url existe, si oui prend la valeur de l'information URL
 if(isset($_GET['page'])){
 
-    $page = $_GET['page'];
+    //on verifie que la clef esiste bien dans mon tableau $ar_pages_var
+    if(array_key_exists($_GET['page'], $ar_pages_var)){
 
-}else{
+        $page = $_GET['page'];
 
-    $page = 'accueil';
-
+    }
 }
 
+//contenu de variable en fonction de 'page'
+$title = $ar_pages_var[$page]['title'];
+$description = $ar_pages_var[$page]['description'];
+$keywords = $ar_pages_var[$page]['keywords'];
 
+//les includes de contenu layout
 include('./includes/layout/header.php');
 
 include('./includes/pages/'.$page.'.php');
