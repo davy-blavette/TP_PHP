3e54a5924f981f46f628ffb6cab61c3425ac17d5
Davy Blavette <davy.blavette@akairnet.com>
2020-06-30 09:11:49 +0200

    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);

--- cda03.s1.2isa.org/config/config_exemple_.php
+++ cda03.s1.2isa.org/config/config_exemple_.php
@@ -9,6 +9,8 @@ $bdd = new PDO(
 
 //Server local dev
 /*
+//Server local dev
+//$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 $bdd = new PDO(
     'mysql:host=localhost:3308;dbname=cda03-bd1;charset=utf8',
     'root',
